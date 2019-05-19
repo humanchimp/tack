6a0946a561436ac353b12a9309b740bb572297fa
Christopher Thorn <chris.thorn@lytics.com>
2019-05-19 10:45:45 -0700

    make these consistent

--- src/findLast.ts
+++ src/findLast.ts
@@ -1,6 +1,6 @@
 export function findLast<T>(
-  array: any[],
   predicate: (it: T, index: number) => boolean,
+  array: any[],
 ): T {
   return array.reduceRight(
     (memo, item, index) => (predicate(item, index) ? memo || item : memo),
--- src/partition.ts
+++ src/partition.ts
@@ -1,8 +1,8 @@
 export function partition<T>(
   predicate: (item: T, index: number) => boolean,
-  collection: T[],
+  array: T[],
 ): [T[], T[]] {
-  return collection.reduce(
+  return array.reduce(
     (memo, item, index) => {
       memo[predicate(item, index) ? 0 : 1].push(item);
       return memo;
